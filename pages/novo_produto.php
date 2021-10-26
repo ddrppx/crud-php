986ac5e4e93081cc0b2d7a7b85e887106b021bca
Pedro <ddrppx@gmail.com>
2021-10-26 12:46:56 -0300

    Pagina de cadastro de produto

---
+++ pages/novo_produto.php
@@ -0,0 +1,59 @@
+<?php
+    require('../database/connect.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS Externo -->
+    <link rel="stylesheet" href="../css/main.css">
+    <!-- JS Externo -->
+    <script src="../js/main.js"></script>
+
+    <title>Novo Produto</title>
+</head>
+<body>
+    <h1>Cadastro de Produto</h1>
+
+    <form method="post" id="form_produto" name="form_produto" action="database/cadastrar_produto.php">
+
+        <label for="nome">Nome</label> <br />
+        <input id="nome" name="nome" type="text"  placeholder="Nome do produto"  />
+        <br /> <br />
+
+        <label for="cor">Cor</label> <br />
+        
+        <?php 
+            $sql = "SELECT idcor, cor FROM cores";
+            $q = mysqli_query($connect, $sql);   
+            if(mysqli_num_rows($q) > 0){
+        ?>
+            <select id="cor" name="cor"> 
+                <option selected disabled>Selecione...</option>
+        <?php
+                while($r = mysqli_fetch_array($q)){
+                    ?>
+                    <option value="<?= $r['idcor'] ?>"><?= $r['cor'] ?></option>
+                    <?php 
+                }
+            }
+        ?>
+            </select>
+        <br /> <br />
+
+        <label for="preco">Preço</label> <br />
+        <input id="preco" name="preco" type="number" step=".01" placeholder="0,00"/>
+        <br /> <br />
+
+        <button type="submit" onclick="return validaSubmitProduto()" id="cadastrar" name="cadastrar" value="1">Cadastrar</button>
+    <form>
+
+    <!-- JS Externo -->
+    <script src="../js/novo_produto.js"></script> 
+
+</body>
+</html>
