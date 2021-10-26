9c7586da3840142a20b02e2ff0c177f1f7d6bd6a
Pedro <ddrppx@gmail.com>
2021-10-26 15:45:39 -0300

    criacao de produto

--- post/request_produto.php
+++ post/request_produto.php
@@ -1,6 +1,45 @@
 <?php 
-    $dados = print_r($_POST, true);
+require('../database/connect.php');
+    $metodo = $_POST['metodo'];
+    $msg = "";
 
-    $output = ['mensagem'=> $dados];
+    switch($metodo){
+        case 1:
+            $nome = addslashes($_POST['nome']);
+
+            // Validar cor como inteiro
+            if (filter_var($_POST['cor'], FILTER_VALIDATE_INT)){
+               $cor = $_POST['cor']; 
+            }
+
+            // Validar preco como float
+
+            if (filter_var($_POST['preco'], FILTER_VALIDATE_FLOAT)){
+                $preco = str_replace(',', '.',$_POST['preco']);
+            }
+
+            if(!empty($nome) && !empty($cor) && !empty($preco)){
+                $insert = "INSERT INTO produtos (nome, cor) VALUES ('$nome', '$cor')";
+                $INSERT = mysqli_query($connect, $insert);
+
+                if($INSERT){
+                    $id_produto = mysqli_insert_id($connect);
+                  
+                    $insert_preco = "INSERT INTO precos (idprod, preco) VALUES($id_produto, $preco)";
+                    $INSERT_PRECO = mysqli_query($connect, $insert_preco);
+                    $success = 1;
+                }
+            } 
+
+            $msg .= "Produto $nome cadastrado com sucesso!";
+
+            break;
+        default:
+            $msg = "Houve um erro...";
+            $success = 0;
+            break;
+    }
+
+    $output = ['success'=> $success, 'mensagem'=> $msg];
     echo json_encode($output);
 ?>
