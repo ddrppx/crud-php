e3ae758d4d003e102dc889c9ab1f6704c3583bca
Pedro <ddrppx@gmail.com>
2021-10-26 00:51:38 -0300

    Configs. do banco

---
+++ database/db_config.php
@@ -0,0 +1,10 @@
+<?php 
+    
+    # Armazena as configuraçoes do banco de dados
+    # (Mais facil de mudar posteriormente)
+    const SERVER_NAME = "localhost";
+    const USER_NAME = "root";
+    const PASSWORD = "";
+    const DB_NAME = "teste";
+
+?>
