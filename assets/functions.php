42cb858510b7c14371885bc1ed06ade6a20b57c1
Pedro <ddrppx@gmail.com>
2021-10-27 00:15:56 -0300

    FUNCTIONS!

---
+++ assets/functions.php
@@ -0,0 +1,36 @@
+<?php
+
+    function resolvePreco($cor, $preco){
+        /*
+        Azul, Vermelho: 20%
+        Amarelo: 10%
+        Vermelho, R$ > 50: 5% 
+        */
+        
+        /* 
+        1 - Amarelo
+        2 - Azul
+        3 - Vermelho
+        */ 
+        switch($cor){
+            // Amarelo
+            case 1:
+                $desconto = 0.1;
+                break;
+            // Azul
+            case 2:
+                $desconto = 0.2;
+                break;
+            //Vermelho
+            case 3:
+                if($preco > 50){
+                    $desconto = 0.05; 
+                } else {
+                    $desconto = 0.2;
+                }
+                break;
+        }
+        $valor = ($preco - ($preco * $desconto));
+        return $valor;
+    }
+?>
