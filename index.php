5d97fb90386aaa4aa24b14be21830de7e5ead1a4
Pedro <ddrppx@gmail.com>
2021-10-27 01:49:17 -0300

    Busca

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
     require_once "database/connect.php";
+    require_once "assets/functions.php";
 ?>
 
 <!DOCTYPE html>
@@ -17,13 +18,22 @@
 <body>
     <h2>Produtos</h2>
     <?php
-        $sql = "SELECT p.idprod as id, p.nome, pc.preco, c.cor FROM produtos p JOIN cores c ON p.cor = c.idcor JOIN precos pc ON pc.idprod = p.idprod";
+        $sql = "SELECT p.idprod as id, p.nome, pc.preco, c.cor, c.idcor FROM produtos p JOIN cores c ON p.cor = c.idcor JOIN precos pc ON pc.idprod = p.idprod";
         $q = mysqli_query($connect, $sql);
 
         if (mysqli_num_rows($q) == 0){
             echo "A lista de produtos esta vazia<br>";
         } else {
     ?>
+
+<fieldset>
+    <legend>Filtros</legend>
+    <input type="text" id="busca" name="busca" placeholder="Buscar produtos">
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email">
+    <input type="submit" value="Filtrar" onclick="buscar()">
+  </fieldset>
+
     <table class="produtos">
         <thead>
             <tr>
@@ -34,16 +44,17 @@
             </tr>
         </thead>
 
-        <tbody>
+        <tbody id="conteudo">
             <?php 
                     $param = base64_encode('id');
                     while ($res = mysqli_fetch_array($q)){
                         $id_64 = base64_encode($res['id']);
+                        $preco = resolvePreco($res['idcor'], $res['preco']);
                         ?>
                         <tr>
                             <td><?= $res['nome'] ?></td>
                             <td><?= $res['cor'] ?></td>
-                            <td class="number">R$ <?= number_format($res['preco'], 2, ",", ".") ?></td>
+                            <td class="number">R$ <?= number_format($preco, 2, ",", ".") ?></td>
                             <td class="centro"><a href='pages/edit_produto.php?i=<?= $id_64 ?>'>Editar</a>
                             <td class="centro"><a href="#" onclick="apagarProd('post/request_produto.php', <?= $res['id'] ?>, '<?= $res['nome'] ?>', 'index.php')">Apagar</a>
                         </tr>
--- js/main.js
+++ js/main.js
@@ -29,22 +29,10 @@ function enviarForm(destino, form_id, redirect = undefined) {
     xhttp.send(data);
 }
 
-    // Apagar produto
-function apagarProd(destino, id, nome, redirect){
-
-    // Pede confirmação diante à exclusão
-    var r = confirm("Deseja realmente excluir o produto: " + nome+ "?");
-    if(!r){
-        return;
-    }
-
+    // Permite enviar dados com form feito por meio do FormData
+function enviarFormManual(destino, formData, callback){
     var xhttp = new XMLHttpRequest();
     var url = destino;
-        
-    // Formulario à ser enviado
-    var data = new FormData();
-    data.append('metodo', 3);
-    data.append('id', id);
 
     xhttp.overrideMimeType("application/json");  
     // Tipo de requisicao
@@ -53,18 +41,95 @@ function apagarProd(destino, id, nome, redirect){
     xhttp.onreadystatechange = function() {
         // Caso a requisiçao volte sucesso
         if (this.readyState == 4 && this.status == 200) {
-            console.log(response);
-          var response = JSON.parse(xhttp.responseText);
+            var response = JSON.parse(xhttp.responseText);
+            callback(response);
+        };
+    } 
+    xhttp.send(formData);
 
-          console.log(response);
+}
+
+    // Apagar produto
+function apagarProd(destino, id, nome, redirect){
+
+    // Pede confirmação diante à exclusão
+    var r = confirm("Deseja realmente excluir o produto: " + nome+ "?");
+
+    if(r){
+        // Formulario à ser enviado
+        var data = new FormData();
+        data.append('metodo', 3);
+        data.append('id', id);
+
+        // Funçao callback para lidar com os valores da função assincrona
+        function callback(response, redirect){
+
             // Mostra um alerta com a resposta do servidor
-          alert(`Produto: ${nome}; ${response.mensagem}.`);
+            alert(`Produto: ${nome} ${response.mensagem}`);
 
             if (redirect && response.success !== 0){
                 window.location.href = redirect;    
             }
         }
-      };
-      xhttp.send(data);
+
+        // Chamando a função
+        // Destino da requisição
+        // Formulario
+        // Função de callback
+        enviarFormManual(destino, data, callback);
+     
+        
+    }
+
 }
 
+function buscar(){
+    var destino = "post/request_produto.php";
+    var busca = document.getElementById('busca');
+    console.log(busca.value);
+
+    
+    // Formulario à ser enviado
+    var data = new FormData();
+    data.append('metodo', 4);
+    data.append('busca', busca.value);
+
+    function callback(response) {
+        var html = "";
+        for(var i = 0; i < response.dados.length; i++){
+            var produto = response.dados[i];
+            html += formatarHTML(produto);
+        }
+        inserirHTML('conteudo', html);
+    }
+
+    enviarFormManual(destino, data, callback);
+}
+
+    // Formata de objeto para table row/table data, para inserir na tabela html 
+function formatarHTML(item){
+    // Link para edição
+    // btoa = base64_encode
+    var editar = `pages/edit_produto.php?i=${btoa(item.id)}`;
+    var apagar = `apagarProd('post/request_produto.php', '${item.id}', '${item.nome}', 'index.php')`;
+    console.log(item.preco);
+    var preco = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRA', currencyDisplay: 'symbol' }).format(item.preco); 
+    preco = preco.replace("BRA", "");
+    var html = `
+        <tr>
+            <td>${item.nome}</td>
+            <td>${item.cor}</td>
+            <td class="number">R$${preco}</td>
+            <td class="centro"><a href="${editar}">Editar</td>
+            <td class="centro"><a href="#" onclick="${apagar}">Apagar</td>
+        </tr>`;
+
+    return html;
+}
+
+    // Insere na table html
+function inserirHTML(id, html){
+    var html_element = document.getElementById(id);
+
+    html_element.innerHTML = html;
+}
